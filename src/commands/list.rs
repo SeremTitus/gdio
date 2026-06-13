704345c00227595ddf69c9a8fe7270f5a0dbb3a1
Serem Titus <seremexclusive@gmail.com>
2026-06-13 09:30:00 +0300

    Add: list command for registered editors

---
+++ src/commands/list.rs
@@ -0,0 +1,27 @@
+use anyhow::Result;
+use crate::config::Config;
+
+pub fn run(config: &Config) -> Result<()> {
+    if config.editors.is_empty() {
+        println!("No editors registered.");
+        println!("Use `gdio add <version>` to download an editor.");
+        return Ok(());
+    }
+
+    println!("{:<35} Path", "Name (version)");
+    println!("{}", "-".repeat(100));
+
+    let mut editors: Vec<_> = config.editors.values().collect();
+    editors.sort_by(|a, b| a.version.cmp(&b.version));
+
+    for editor in editors {
+        println!(
+            "{:<35} {}",
+            format!("{} ({})", editor.name, editor.version),
+            editor.path.display()
+        );
+    }
+
+    println!("\nTotal: {} editor(s)", config.editors.len());
+    Ok(())
+}
