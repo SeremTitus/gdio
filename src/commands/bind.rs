eba3955f65cbb58eed26cfa3accff1fcfcfcfb1a
Serem Titus <seremexclusive@gmail.com>
2026-08-10 14:25:00 +0300

    Replace SystemTime unwrap() with unwrap_or_default() to prevent panic

--- src/commands/bind.rs
+++ src/commands/bind.rs
@@ -75,7 +75,7 @@ pub fn run(target: Option<&str>, config: &mut Config) -> Result<()> {
 
     let now = SystemTime::now()
         .duration_since(UNIX_EPOCH)
-        .unwrap()
+        .unwrap_or_default()
         .as_secs()
         .to_string();
 
